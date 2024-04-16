44f4dfa5917b588d3f8bcd1deb23115f44212b86
farhan-mazumder <farhanmazumder75@gmail.com>
2024-04-16 20:35:16 +0600

    added font family and tailwind configuration.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "playfair-display": "'Playfair Display', serif",
+        poppins: "'Poppins', sans-serif",
+      },
+      colors: {
+        primary: "#FCA22A",
+        title: "#de385d",
+        natural: "#0e385d",
+        navey: "#0e385d",
+        green: "#b3b3b3",
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1.5rem",
+        // md: "2rem",
+        // lg: "4rem",
+        // xl: "5rem",
+        // "2xl": "6rem",
+      },
+    },
   },
   plugins: [],
 };
