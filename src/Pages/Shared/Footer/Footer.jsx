764f7c91c437db6fdb3ba8799ad253c0ad20477b
farhan-mazumder <farhanmazumder75@gmail.com>
2024-04-17 14:23:08 +0600

    Footer has been added with responsiveness.

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,5 +1,32 @@
 const Footer = () => {
-  return <></>;
+  return (
+    <>
+      <footer className="p-10 bg-neutral">
+        <div className="container max-w-[1280px] footer text-neutral-content">
+          <nav className="">
+            <h6 className="footer-title">Services</h6>
+            <a className="link link-hover">Branding</a>
+            <a className="link link-hover">Design</a>
+            <a className="link link-hover">Marketing</a>
+            <a className="link link-hover">Advertisement</a>
+          </nav>
+          <nav>
+            <h6 className="footer-title">Company</h6>
+            <a className="link link-hover">About us</a>
+            <a className="link link-hover">Contact</a>
+            <a className="link link-hover">Jobs</a>
+            <a className="link link-hover">Press kit</a>
+          </nav>
+          <nav>
+            <h6 className="footer-title">Legal</h6>
+            <a className="link link-hover">Terms of use</a>
+            <a className="link link-hover">Privacy policy</a>
+            <a className="link link-hover">Cookie policy</a>
+          </nav>
+        </div>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
